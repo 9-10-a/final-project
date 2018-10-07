3c8e3a9ce2780ddaa04ff568979411e51a1b5c9f
Will Harris <will@lifenet.net>
2018-10-07 11:10:15 -0700

    changed header look and added link to profile page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,20 @@ import { AuthGuard } from './users/auth.guard';
 import { LogListComponent } from './logs/log-list/log-list.component';
 import { LogCreateComponent } from './logs/log-create/log-create.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
-  { path: '', component: HomepageComponent },
+  { path: 'home', component: HomepageComponent },
   { path: 'log', component: LogListComponent },
   { path: 'create', component: LogCreateComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {
     path: 'edit/:logId',
     component: LogCreateComponent,
     canActivate: [AuthGuard]
   },
-  { path: 'auth', loadChildren: './users/auth.module#AuthModule' }
+  { path: 'auth', loadChildren: './users/auth.module#AuthModule' },
+  { path: 'profile', component: ProfileComponent}
 ];
 
 @NgModule({
