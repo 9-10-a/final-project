0d5f006483ecd12ed51915b6f8e2e54d486bc1ec
Regina Clifford <ginamarion.clifford@gmail.com>
2018-10-15 13:57:37 +0300

    map component working - no errors

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,10 +7,47 @@ import { MouseEvent } from '@agm/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent {
+  markers: Marker[] = [
+    {
+      lat: 40.5817815,
+      lng: -122.3559081,
+      name: 'Planet Fitness',
+      draggable: false,
+    },
+    {
+      lat: 40.5571184,
+      lng: -122.3067689,
+      name: 'Sun Oaks Tennis and Fitness',
+      draggable: false,
+    },
+    {
+      lat: 40.5840854,
+      lng: -122.3749692,
+      name: 'Shasta Athletic Club',
+      draggable: false,
+    },
+    {
+      lat: 40.5813204,
+      lng: -122.3930377,
+      name: 'Align',
+      draggable: false,
+    },
+    {
+      lat: 40.5732405,
+      lng: -122.3748009,
+      name: 'Monster Fitness',
+      draggable: false,
+    },
+    {
+      lat: 40.5672764,
+      lng: -122.362331,
+      name: 'GCD Fitness',
+      draggable: false,
+    }
+  ];
   // initial position for map - Redding, California
   lat = 40.583333;
   lng = -122.366667;
-
   // google maps zoom level
   zoom = 13;
 
@@ -19,65 +56,29 @@ export class MapComponent {
     this.longitude = event.coords.lng;
   }*/
 
-  /*clickedMarker(label: string, index: number) {
-    console.log(`clicked the marker: ${label || index}`)
-  }*/
-  mapClicked($event: MouseEvent) {
-    this.markers.push({
-      lat: $event.coords.lat,
-      lng: $event.coords.lng,
-      draggable: true
-    });
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
   }
 
   markerDragEnd(m: Marker, $event: MouseEvent) {
     console.log('dragEnd', m, $event);
   }
-
-  markers: Marker[] = [
-    {
-      lat: 40.5817815,
-      lng: -122.3559081,
-      label: 'Planet Fitness',
-      draggable: false
-    },
-    {
-      lat: 40.5571184,
-      lng: -122.3067689,
-      label: 'Sun Oaks Tennis and Fitness',
-      draggable: false
-    },
-    {
-      lat: 40.5840854,
-      lng: -122.3749692,
-      label: 'Shasta Athletic Club',
-      draggable: false
-    },
-    {
-      lat: 40.5813204,
-      lng: -122.3930377,
-      label: 'Align',
-      draggable: false
-    },
-    {
-      lat: 40.5732405,
-      lng: -122.3748009,
-      label: 'Monster Fitness',
-      draggable: false
-    },
-    {
-      lat: 40.5672764,
-      lng: -122.362331,
-      label: 'GCD Fitness',
-      draggable: false
-    }
-  ];
 }
 
 // just an interface for type safety.
 interface Marker {
   lat: number;
   lng: number;
-  label?: string;
+  name: string;
   draggable: boolean;
 }
+
+
+/*mapClicked($event: MouseEvent) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng,
+      draggable: false,
+    });
+  }*/
+
